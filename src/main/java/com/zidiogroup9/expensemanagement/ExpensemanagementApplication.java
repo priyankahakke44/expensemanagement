93894881c1bdffedb2d7541d9ca508c259da7eb4
Yogesh Chaudhari <chaudhariyogesh2408@gmail.com>
2025-04-02 18:28:52 +0530

    Patch/Removed loging statements

--- src/main/java/com/zidiogroup9/expensemanagement/ExpensemanagementApplication.java
+++ src/main/java/com/zidiogroup9/expensemanagement/ExpensemanagementApplication.java
@@ -8,7 +8,6 @@ public class ExpensemanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensemanagementApplication.class, args);
-		System.out.println("Simple string");
 	}
 
 }
