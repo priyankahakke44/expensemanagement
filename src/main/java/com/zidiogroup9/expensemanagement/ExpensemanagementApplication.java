29fdb78a5d1b88e8789fb0e07a695059e546103f
Priyanka <priyankahakke44@gmail.com>
2025-04-02 14:26:42 +0800

    Add initial files

---
+++ src/main/java/com/zidiogroup9/expensemanagement/ExpensemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.zidiogroup9.expensemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensemanagementApplication.class, args);
+	}
+
+}
