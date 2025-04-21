9225bfc5c555113fdbe57bf7cc1badc6a0221ae4
Yogesh Chaudhari <chaudhariyogesh2408@gmail.com>
2025-04-21 19:31:49 +0530

    User Pagination and Sorting

--- src/main/java/com/zidiogroup9/expensemanagement/controller/UserController.java
+++ src/main/java/com/zidiogroup9/expensemanagement/controller/UserController.java
@@ -1,69 +1,52 @@
 package com.zidiogroup9.expensemanagement.controller;
 
-
 import com.zidiogroup9.expensemanagement.advices.ApiResponse;
 import com.zidiogroup9.expensemanagement.dtos.ChangePasswordDto;
-import com.zidiogroup9.expensemanagement.dtos.UserDto;
-import com.zidiogroup9.expensemanagement.services.AuthService;
-import com.zidiogroup9.expensemanagement.services.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/user")
-@RequiredArgsConstructor
-public class UserController {
-    private final UserService userService;
-    @GetMapping(path = "/profile")
-    public ResponseEntity<UserDto> getProfile(){
-        return ResponseEntity.ok(userService.getProfile());
-    }
-    @PatchMapping(path = "/changePassword")
-    public ResponseEntity<ApiResponse<?>> changePassword(@RequestBody ChangePasswordDto changePasswordDto){
-        userService.changePassword(changePasswordDto);
-        ApiResponse<String> response = new ApiResponse<>("Password changed successfully");
-        return ResponseEntity.ok(response);
-    }
-}
-package com.zidiogroup9.expensemanagement.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.zidiogroup9.expensemanagement.dtos.UpdateUserDto;
 import com.zidiogroup9.expensemanagement.dtos.UserDto;
-import com.zidiogroup9.expensemanagement.entities.User;
-import com.zidiogroup9.expensemanagement.services.Impl.AuthServiceImpl;
-import com.zidiogroup9.expensemanagement.services.Impl.UserServiceimpl;
+
+
+import com.zidiogroup9.expensemanagement.services.Impl.UserServiceImpl;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-import org.apache.catalina.mapper.Mapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
-	private final UserServiceimpl userServiceimpl;
+	private final UserServiceImpl userServiceImpl;
 	private final ModelMapper mapper;
+
+	@GetMapping(path = "/profile")
+	public ResponseEntity<UserDto> getProfile() {
+		return ResponseEntity.ok(userServiceImpl.getProfile());
+	}
+
+	@PatchMapping(path = "/changePassword")
+	public ResponseEntity<ApiResponse<?>> changePassword(@RequestBody ChangePasswordDto changePasswordDto) {
+		userServiceImpl.changePassword(changePasswordDto);
+		ApiResponse<String> response = new ApiResponse<>("Password changed successfully");
+		return ResponseEntity.ok(response);
+	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDto> findUserById(@PathVariable(name = "id") String id, HttpServletResponse response) {
-		UserDto userById = userServiceimpl.findUserById(id);
+		UserDto userById = userServiceImpl.findUserById(id);
 
 		return new ResponseEntity<UserDto>(userById, HttpStatus.OK);
 
@@ -72,7 +55,7 @@ public class UserController {
 	@DeleteMapping("/{id}")
 	private ResponseEntity<String> deleteUser(@PathVariable(name = "id") String id,
 			HttpServletResponse httpServletResponse) {
-		UserDto deleteUser = userServiceimpl.deleteUser(id);
+		UserDto deleteUser = userServiceImpl.deleteUser(id);
 		if (deleteUser != null) {
 			mapper.map(deleteUser, String.class);
 			return new ResponseEntity<String>("User deleted", HttpStatus.OK);
@@ -82,12 +65,22 @@ public class UserController {
 	}
 
 	@PutMapping("update/{id}")
-	private ResponseEntity<UserDto> updateUser(@PathVariable(name = "id") String id,@RequestBody UpdateUserDto updateUserDto, HttpServletResponse response) {
-		UserDto updatedUser = userServiceimpl.updateUser(id,updateUserDto);
+	private ResponseEntity<UserDto> updateUser(@PathVariable(name = "id") String id,
+			@RequestBody UpdateUserDto updateUserDto, HttpServletResponse response) {
+		UserDto updatedUser = userServiceImpl.updateUser(id, updateUserDto);
 		if (updatedUser != null) {
 			return new ResponseEntity<UserDto>(updatedUser, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<UserDto>(updatedUser, HttpStatus.NOT_FOUND);
 		}
 	}
+
+	@GetMapping("all/{pageNo}/{noOfRecords}")
+	public ResponseEntity<List<UserDto>> findAllUsers(@PathVariable(name = "pageNo") int pageNo,
+			@PathVariable(name = "noOfRecords") int noOfRecords, HttpServletResponse response) {
+		
+		List<UserDto> findall = userServiceImpl.findall(pageNo,noOfRecords);
+		return new ResponseEntity<List<UserDto>>(findall, HttpStatus.FOUND);
+	}
+
 }
